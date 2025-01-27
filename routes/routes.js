79acc28c2ffcf68e35e668cafe2372d650944d0d
Uugantogtokh <uugantogtokh.otgonbaatar@unimedia.mn>
2025-01-27 13:51:17 +0800

    fix

--- routes/routes.js
+++ routes/routes.js
@@ -4,16 +4,16 @@ const timeController = require("../controllers/timeController");
 const userController = require("../controllers/userController");
 
 // Time routes
-router.post("/time/start", timeController.startWork); // Start work for a user
-router.post("/time/:user_id/end", timeController.endWork); // End work for a user
-router.get("/time/user/:user_id", timeController.getTimeByUserId); // Get all time entries for a user
-router.get("/time/date/:created_date", timeController.getAllTimeByDate); // Get time entries by date
+router.post("/api/time/start", timeController.startWork); // Start work for a user
+router.post("/api/time/:user_id/end", timeController.endWork); // End work for a user
+router.get("/api/time/user/:user_id", timeController.getTimeByUserId); // Get all time entries for a user
+router.get("/api/time/date/:created_date", timeController.getAllTimeByDate); // Get time entries by date
 
 // User routes
-router.get("/users", userController.getAllUsers); // Get all users
-router.post("/users", userController.createUser); // Create a new user
-router.get("/users/:user_id", userController.getUserById); // Get user by ID
-router.put("/users/:user_id", userController.updateUser); // Update user by ID
-router.delete("/users/:user_id", userController.deleteUser); // Delete user by ID
+router.get("/api/users", userController.getAllUsers); // Get all users
+router.post("/api/users", userController.createUser); // Create a new user
+router.get("/api/users/:user_id", userController.getUserById); // Get user by ID
+router.put("/api/users/:user_id", userController.updateUser); // Update user by ID
+router.delete("/api/users/:user_id", userController.deleteUser); // Delete user by ID
 
 module.exports = router;
